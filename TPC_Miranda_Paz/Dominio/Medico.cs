8dd5d4889b704013c0113db25ef1a8b69910b09c
gaspaz <gaston.paz@alumnos.frgp.utn.edu.ar>
2021-06-12 16:18:22 -0300

    Se crea proyecto y modelo de dominio
    Se crea proyecto y modelo de dominio

---
+++ TPC_Miranda_Paz/Dominio/Medico.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    public class Medico : Usuario
+    {
+
+        public Especialidad Especialidad { get; set; }
+
+        public string Martricula { get; set; }
+
+        public int Id { get; set; }
+
+        public bool[] Agenda { get; set; }
+       
+
+    }
+}
