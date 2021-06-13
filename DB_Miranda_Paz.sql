5b603ad6036a2bb80dec0e3796096519c3a82bf4
gaspaz <gaston.paz@alumnos.frgp.utn.edu.ar>
2021-06-13 20:35:03 -0300

    DB
    DB
    cargo medicos, recepcionistas y admin

--- DB_Miranda_Paz.sql
+++ DB_Miranda_Paz.sql
@@ -25,6 +25,48 @@ CREATE TABLE Medicos(
 	/*AGENDA*/
 )
 GO
+INSERT INTO Medicos
+VALUES ('Joaquin', 'Achaval Duria', 'M116688' , '1', 'AchavalDuria@Joaquin.com', 'joacoduria', '98975462', '(387) 962-2847'),
+('Isidoro', 'Arostegui', 'M115544', '1','Aros@Isidoro.com', 'isidorotegui', '54782136','(695) 920-0076'),
+('Elizabeth Carolina', 'Ayala', 'M112244', '2', 'eayala@google.com', 'eliaya', '33225588', '(695) 920-0054'),
+('Alejandro Agustin', 'Badano', 'M659878', '2', 'agustin@badano.com', 'alebadano', '40578945','(357) 874-7957'),
+('Paola Elizabeth', 'Barrios', 'M221168', '3','pneighborhood@me.dev', 'paobarrios', '35410621','(946) 907-8987'),
+('Lucas', 'Crucci', 'M871547', '3','lcrucci@hotmail.com.pe', 'crucelucas', '32568974','(389) 615-9729'),
+('Emiliano', 'Del Pino', 'M789456', '4','efromthepine@notmyrealmail.com', 'emidelpi', '45678912','(368) 886-5181'),
+('Federico Damian', 'Di Fulvio', 'M547812',  '4', 'fdifulvio@gmail.com', 'fedediful', '78956412', '(368) 886-5879'),
+('Matias Andres', 'Fahler', 'M123265', '5', 'matifa@yahoo.com', 'fahmati', '12457896', '(186) 828-6592'),
+('Bruno Alexis', 'Ferrante', 'M748745', '5', 'ferrante@alexis.com', 'ferralexis', '45871236', '(926) 973-5945'),
+('Alexis Damian', 'Figueira', 'M147852', '6','afigueira@hotmail.com.uy', 'alexfigue', '37896254', '(926) 973-5978'),
+('Cecilia Verónica', 'Galarza', 'M789147', '6', 'cgalarza@utn.edu.ar', 'cecigala', '12301478', '(926) 974-1245'),
+('Alejandro Matias', 'Gazzo', 'M352698', '7', 'agazzo@notmyrealmail.com', 'alegazz', '32353631','(926) 547-1324'),
+('Nahuel Alejandro', 'Godoy', 'M878984', '7', 'ngodoy@google.com', 'godona', '45625897', '(926) 589-7894'),
+('Edgardo Simon', 'Gonzalez', 'M123456', '8', 'egonzalez@fakemail.com', 'sigon', '14774125', '(926 789-1478)'),
+('Pablo Agustín', 'Ibazeta', 'M963258', '8', 'pibazeta@google.com', 'ibazagus', '84269357','(578) 581-0349'),
+('Javier Agustin', 'Larroca', 'M159753', '9', 'jlarroca@hotmail.com.ar', 'javiagula', '19786247','(723) 346-3332'),
+('Yesica Regina', 'Laurentino Goncalves', 'M756845', '9', 'ylaurentino@independiente.com', 'regilau', '65265398','(101) 716-0947'),
+('Zair Andres', 'Mar Cardozo', 'M873214', '10', 'zmar@hotmail.com.uy', 'zamarca', '35231456','(203) 991-1918'),
+('Jonathan Daniel', 'Martinez', 'M852258', '10', 'danimartinez@trucho.com', 'martidani', '75454789', '(328) 853-6797'),
+('German Emanuel', 'Mena', 'M6852145', '11', 'menager@elmail.com', 'germanuel', '65124578', '(859) 564-9626'),
+('Maximiliano Guillermo', 'Miranda', 'M897465', '11', 'mmiranda@hotmail.com.ar', 'soymaxi', '36546852','(346) 791-8453'),
+('Bruno Mauricio', 'Molteni', 'M714724', '12', 'bmolteni@hotmail.com.uy', 'soybruno', '32313233', '(346) 741-2589'),
+('Ignacio', 'Petrignani Castro', 'M138726', '12', 'ipetrignani@notmyrealmail.com', 'ignasoy', '12315978','(558) 813-4402'),
+('Valentin', 'Plaza', 'M639846', '13', 'vplaza@notmyrealmail.com', 'valensoy', '45666666','(779) 742-0557'),
+('Alexander Alberto', 'Popp', 'M157486', '13', 'apopp@google.com', 'popis', '45555444','(488) 244-5467'),
+('Matias Joel', 'Quintana Soria', 'M111111', '14', 'mquintana@hotmail.com.pe', 'quintaso', '99888777','(669) 213-0345'),
+('Leonardo Ezequiel', 'Alpuin Schunk', 'M116699', '14', 'AlpuinLeo@Schunk.com', 'leoezequiel', '98987879', '(387) 962-2457'),
+('Adriel Elian', 'Rasjido', 'M221121', '15', 'arasjido@frgp.utn.edu.ar', 'adriras', '25444666', '(332) 7-4438'),
+('Agustin Lautaro', 'Robles', 'M221111', '15', 'arobles@hotmail.com.ar', 'laurob', '25545654','(970) 885-0929'),
+('Matias', 'Sanchez Villar', 'M223122', '16', 'msanchez@fakemail.com', 'matisanvi', '30251478','(533) 285-8591'),
+('Leonardo Martin', 'Scalesi', 'M663355', '16', 'leomartin@outlook.ra', 'calesi', '32111333', '(806) 630-4551'),
+('Tomas', 'Scutti', 'M161748', '17', 'tscutti@notmyrealmail.com', 'cutito', '32333444','(169) 249-0460'),
+('Walter Emmanuel', 'Stamm', 'M888999', '17', 'wstamm@argentina.ar', 'tamwal', '35566988', '(332) 789-4438'),
+('Mathias Leandro', 'Tejeda', 'M774411', '18', 'mtejeda@hotmail.com', 'tejelean', '45698896','(963) 306-5247'),
+('Daniel Alejandro', 'Trunso', 'M411511', '18', 'trunso@fotolog.com', 'fotoale', '45784147', N'(977) 335-7032'),
+('Elmer Dennis', 'Vasquez Quispe', 'M655453', '19', 'elmervas@elmail.com', 'soyelmer', '36999888', '(324) 635-0486'),
+('Sofia Noemi', 'Vigliaccio', 'M444123', '19', 'svigliaccio@frgp.utn.edu.ar', 'sofisoy', '44321581','(564) 377-2363'),
+('Alejandro', 'Virasoro', 'M688978', '20', 'avirasoro@hotmail.com.ar', 'virajandro', '37548788', '(789) 789-2541'),
+('Luciano Federico', 'Yomayel', 'M111111', '20', 'lyomayel@hotmail.com', 'luchiano', '25631125','(625) 229-4746')
+GO
 CREATE TABLE Recepcionista(
 	Id INT PRIMARY KEY NOT NULL IDENTITY(1,1),
 	Nombre VARCHAR (60) NOT NULL,
@@ -35,6 +77,11 @@ CREATE TABLE Recepcionista(
 	Telefono VARCHAR (20) NULL
 )
 GO
+INSERT INTO Recepcionista
+VALUES ('Florencia', 'Rodriguez', 'flor-rodriguez@mail.com', '123456', '37059672', '1144558899'),
+('Agustina', 'Perez', 'aguspe@mail.com', '789456', '37059678', '1155447788'),
+('Rodrigo', 'Alsina', 'rodrial@mail.com', '159753', '35789877', '1122554477')
+GO
 CREATE TABLE Administrador(
 	Id INT PRIMARY KEY NOT NULL IDENTITY(1,1),
 	Nombre VARCHAR (60) NOT NULL,
@@ -44,6 +91,10 @@ CREATE TABLE Administrador(
 	Dni VARCHAR (15) NOT NULL,
 	Telefono VARCHAR (20) NULL
 )
+GO
+INSERT INTO Administrador
+VALUES ('Gastón', 'Paz', 'gastonpaz@mail.com', 'gaspaz', '38013023', '1165656565'),
+('Maximiliano', 'Miranda','maximiranda@mail.com', 'mirama', '38014024', '1145454545')
 GO
 CREATE TABLE Pacientes(
 	Id INT PRIMARY KEY NOT NULL IDENTITY(1,1),
